96440e0c10f894ef59deb45bba17fa849f159241
jeffbrn <jeff.brn@gmail.com>
2023-06-10 19:37:06 -0600

    start csharp impl

---
+++ csharp/csharp/RayTracer/Vector/Tuple.cs
@@ -0,0 +1,75 @@
+﻿using System.Numerics;
+
+namespace Research.RayTracer.Vector {
+	public class RayTuple<T> : IEquatable<RayTuple<T>> where T : INumber<T> {
+		private readonly T _x;
+		private readonly T _y;
+		private readonly T _z;
+		private readonly T _w;
+
+		private RayTuple(T x, T y, T z, T w) {
+			if(w != ZeroNum && w != OneNum) throw new ArgumentOutOfRangeException(nameof(w));
+			_x = x;
+			_y = y;
+			_z = z;
+			_w = w;
+		}
+
+		public T X { get => _x; }
+		public T Y { get => _y; }
+		public T Z { get => _z; }
+		public T W { get => _w; }
+
+		public bool IsPoint => _w == OneNum;
+		public bool IsVector => _w == ZeroNum;
+
+		public double Magnitude() {
+			if (IsPoint) throw new InvalidOperationException("Magnitude is for vectors");
+			return Math.Sqrt((double)Convert.ChangeType(_x * _x + _y * _y + _z * _z, typeof(double)));
+		}
+
+		public RayTuple<double> Normalize() {
+			if (IsPoint) throw new InvalidOperationException("Normailze is for vectors");
+			var mag = Magnitude();
+			return new(Convert.ToDouble(_x)/mag, Convert.ToDouble(_y)/mag, Convert.ToDouble(_z)/mag, 0.0);
+		}
+
+		public T DotProd(RayTuple<T> other) {
+			if (IsPoint || other.IsPoint) throw new InvalidOperationException("Dot Product is for vectors");
+			return _x*other._x + _y*other._y + _z*other._z;
+		}
+
+		public bool Equals(RayTuple<T>? other) {
+			return other != null && _x == other._x && _y == other._y && _z == other._z && _w == other._w;
+		}
+
+		public override bool Equals(object? obj) {
+			return Equals(obj as RayTuple<T>);
+		}
+
+		public override int GetHashCode() => _x.GetHashCode() ^ _y.GetHashCode() ^ _z.GetHashCode() ^ _w.GetHashCode();
+
+		public static RayTuple<TNum> Point<TNum>(TNum x, TNum y, TNum z) where TNum : INumber<TNum> =>
+			new(x, y, z, RayTuple<TNum>.OneNum);
+		public static RayTuple<TNum> Vector<TNum>(TNum x, TNum y, TNum z) where TNum : INumber<TNum> =>
+			new(x, y, z, RayTuple<TNum>.ZeroNum);
+
+		public static RayTuple<T> operator +(RayTuple<T> lhs, RayTuple<T> rhs) => new(lhs._x + rhs._x, lhs._y + rhs._y, lhs._z + rhs._z, lhs._w + rhs._w);
+		public static RayTuple<T> operator -(RayTuple<T> lhs, RayTuple<T> rhs) => new(lhs._x - rhs._x, lhs._y - rhs._y, lhs._z - rhs._z, lhs._w - rhs._w);
+		public static RayTuple<T> operator -(RayTuple<T> rhs) => new(-rhs._x, -rhs._y, -rhs._z, rhs._w);
+		public static RayTuple<T> operator *(RayTuple<T> lhs, T rhs) => new(lhs._x * rhs, lhs._y * rhs, lhs._z * rhs, lhs._w);
+		public static RayTuple<T> operator *(RayTuple<T> lhs, RayTuple<T> rhs) {
+			if (lhs.IsPoint || rhs.IsPoint) throw new InvalidOperationException("Cross Product is for vectors");
+			return new(
+				lhs._y * rhs._z - lhs._z * rhs._y,
+				lhs._z * rhs._x - lhs._x * rhs._z,
+				lhs._x * rhs._y - lhs._y * rhs._x,
+				ZeroNum);
+		}
+
+		public static RayTuple<T> operator /(RayTuple<T> lhs, T rhs) => new(lhs._x / rhs, lhs._y / rhs, lhs._z / rhs, lhs._w);
+
+		private static T ZeroNum => (T)Convert.ChangeType(0, typeof (T));
+		private static T OneNum => (T)Convert.ChangeType(1, typeof(T));
+	}
+}
---
+++ csharp/csharp/RayTracerTests/Vector/Tuples.cs
@@ -0,0 +1,118 @@
+﻿using Research.RayTracer.Vector;
+
+namespace Research.RayTracer.Tests.Vector {
+	public class Tuples {
+		[Fact]
+		public void IsAPoint() {
+			var a = RayTuple<float>.Point(4.3f, -4.2f, 3.1f);
+			Assert.Equal(4.3f, a.X);
+			Assert.Equal(-4.2f, a.Y);
+			Assert.Equal(3.1f, a.Z);
+			Assert.Equal(1.0f, a.W);
+			Assert.True(a.IsPoint);
+			Assert.False(a.IsVector);
+		}
+
+		[Fact]
+		public void IsAVector() {
+			var a = RayTuple<float>.Vector(4.3f, -4.2f, 3.1f);
+			Assert.Equal(4.3f, a.X);
+			Assert.Equal(-4.2f, a.Y);
+			Assert.Equal(3.1f, a.Z);
+			Assert.Equal(0.0f, a.W);
+			Assert.True(a.IsVector);
+			Assert.False(a.IsPoint);
+		}
+
+		[Fact]
+		public void Add() {
+			var a1 = RayTuple<int>.Point(3, -2, 5);
+			var a2 = RayTuple<int>.Vector(-2, 3, 1);
+			var expected = RayTuple<int>.Point(1, 1, 6);
+			Assert.Equal(expected, a1 + a2);
+		}
+
+		[Fact]
+		public void InvalidAdd() {
+			var a1 = RayTuple<int>.Point(3, -2, 5);
+			var a2 = RayTuple<int>.Point(-2, 3, 1);
+			Assert.Throws<ArgumentOutOfRangeException>(() => a1 + a2);
+		}
+
+		[Fact]
+		public void Subtract() {
+			var a1 = RayTuple<int>.Point(3, 2, 1);
+			var a2 = RayTuple<int>.Point(5, 6, 7);
+			var expected1 = RayTuple<int>.Vector(-2, -4, -6);
+			Assert.Equal(expected1, a1 - a2);
+
+			var v2 = RayTuple<int>.Vector(5, 6, 7);
+			var expected2 = RayTuple<int>.Point(-2, -4, -6);
+			Assert.Equal(expected2, a1 - v2);
+		}
+
+		[Fact]
+		public void InvalidSubtract() {
+			var a1 = RayTuple<int>.Vector(3, -2, 5);
+			var a2 = RayTuple<int>.Point(-2, 3, 1);
+			Assert.Throws<ArgumentOutOfRangeException>(() => a1 - a2);
+		}
+
+		[Fact]
+		public void Negate() {
+			var a = RayTuple<int>.Vector(1, -2, 3);
+			var expected = RayTuple<int>.Vector(-1, 2, -3);
+			Assert.Equal(expected, -a);
+		}
+
+		[Fact]
+		public void ScalarMult() {
+			var a = RayTuple<float>.Vector(1.0, -2.0, 3.0);
+			Assert.Equal(RayTuple<float>.Vector(3.5, -7.0, 10.5), a * 3.5);
+			var b = RayTuple<int>.Point(2, 4, 6);
+			Assert.Equal(RayTuple<int>.Point(1, 2, 3), b / 2);
+		}
+
+		[Fact]
+		public void Magnitude() {
+			Assert.Equal(1.0, RayTuple<int>.Vector(0, 1, 0).Magnitude());
+			Assert.Equal(1.0, RayTuple<float>.Vector(0.0, 0.0, 1.0).Magnitude());
+			var expected = Math.Sqrt(14.0);
+			Assert.Equal(expected, RayTuple<int>.Vector(1, 2, 3).Magnitude());
+			Assert.Equal(expected, RayTuple<int>.Vector(-1, -2, -3).Magnitude());
+			Assert.Throws<InvalidOperationException>(() => RayTuple<int>.Point(1, 2, 3).Magnitude());
+		}
+
+		[Fact]
+		public void Normalize() {
+			var v1 = RayTuple<int>.Vector(4, 0, 0);
+			Assert.Equal(RayTuple<double>.Vector(1.0, 0, 0), v1.Normalize());
+			var mag = Math.Sqrt(14.0);
+			var expected = RayTuple<double>.Vector(1.0 / mag, 2.0 / mag, 3.0 / mag);
+			Assert.Equal(expected, RayTuple<float>.Vector(1.0, 2.0, 3.0).Normalize());
+			Assert.Equal(1.0, RayTuple<float>.Vector(1.0, 2.0, 3.0).Normalize().Magnitude());
+			Assert.Throws<InvalidOperationException>(() => RayTuple<int>.Point(1, 2, 3).Normalize());
+		}
+
+		[Fact]
+		public void DotProduct() {
+			var a = RayTuple<int>.Vector(1, 2, 3);
+			var b = RayTuple<int>.Vector(2, 3, 4);
+			Assert.Equal(20, a.DotProd(b));
+			Assert.Throws<InvalidOperationException>(() => RayTuple<int>.Point(1, 2, 3).DotProd(a));
+			Assert.Throws<InvalidOperationException>(() => a.DotProd(RayTuple<int>.Point(1, 2, 3)));
+			Assert.Throws<InvalidOperationException>(() => RayTuple<int>.Point(1, 2, 3).DotProd(RayTuple<int>.Point(1, 2, 3)));
+		}
+
+		[Fact]
+		public void CrossProduct() {
+			var a = RayTuple<int>.Vector(1, 2, 3);
+			var b = RayTuple<int>.Vector(2, 3, 4);
+			Assert.Equal(RayTuple<int>.Vector(-1, 2, -1), a * b);
+			Assert.Equal(RayTuple<int>.Vector(1, -2, 1), b * a);
+			Assert.Throws<InvalidOperationException>(() => RayTuple<int>.Point(1, 2, 3) * a);
+			Assert.Throws<InvalidOperationException>(() => a * RayTuple<int>.Point(1, 2, 3));
+			Assert.Throws<InvalidOperationException>(() => RayTuple<int>.Point(1, 2, 3) * RayTuple<int>.Point(1, 2, 3));
+		}
+	}
+}
